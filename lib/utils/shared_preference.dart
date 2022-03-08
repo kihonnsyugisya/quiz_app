fb1993cb62820d94bb129d316931e6383b8b57ce
田辺 <katuobrothers@icloud.com>
2022-03-08 22:39:36 +0900

    端末依存のデータ保存設定 ②ステータスを保存できるロジックの追加

---
+++ lib/utils/shared_preference.dart
@@ -0,0 +1,36 @@
+
+import 'package:quiz_app/utils/package_info.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreference {
+  SharedPreference._();
+
+  static final _instance = SharedPreference._internal();
+
+  SharedPreference._internal();
+
+  factory SharedPreference(){
+    return _instance;
+  }
+
+  SharedPreferences? _prefs;
+  String status = 0.toString();
+
+  init() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  get getStatus {
+    status = _prefs!.getString('status') ?? 0.toString();
+    print('status = ${status}');
+  }
+
+  void setStatus() {
+    _prefs!.setString('status', PackageInfo.version);
+  }
+
+  void getRestStatus() {
+    status = 0.toString();
+  }
+
+}
